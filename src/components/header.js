94a7896f7fae238714c97986c3687902a069377b
sumit m malik <sumit.m.malik@oracle.com>
2018-08-19 01:48:02 +0530

    Header: Draft I

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Header = (props) => {
-	const {text} = props;
-	return (
-		<h1>{text}</h1>
-	);
-};
+import './../Profile.css';
+import {HeaderButtonItem} from './headerButtonItem';
 
-export {Header};
+class Header extends Component {
+    render() {
+        return (
+                <div className="header-bg" >
+                    <div className="flex header-position">
+                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-home" title="Home" msg="Home" />
+                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-bolt" title="Moments" msg="Moments"/>
+                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-bell" title="Notifications" msg="Notifications" />
+                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-envelope" title="Messages" msg="Messages"/>
+                    </div>
+                    <div className="twitter-icon-style">
+                    <i className="fab fa-twitter fa-1x"/>
+                </div>
+                </div>
+                );
+    }
+}
+
+export{ Header};
