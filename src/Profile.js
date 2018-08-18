2523d741397cee264abc9df73085eae9f3224ea7
sumit m malik <sumit.m.malik@oracle.com>
2018-08-19 01:48:29 +0530

    Header: Draft IA

---
+++ src/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import {Header} from './components/header';
+
+class Profile extends Component {
+  render() {
+        return (
+        <Header/>
+    );
+  }
+}
+
+export default Profile;
