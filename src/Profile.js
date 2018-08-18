a2cdccaf0b8c7b51850f9f33ff9237030db0e07a
sumit m malik <sumit.m.malik@oracle.com>
2018-08-19 02:25:54 +0530

    Adding Constants and Messages file in /core

--- src/Profile.js
+++ src/Profile.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 import {Header} from './components/header';
+import * as Messages from './core/UIMessages';
 
 class Profile extends Component {
-  render() {
+    componentDidMount() {
+        document.title = Messages.TITLE;
+    }
+
+    render() {
         return (
-        <Header/>
-    );
-  }
+                <Header/>
+                );
+    }
 }
 
 export default Profile;
--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 
 import './../Profile.css';
 import {HeaderButtonItem} from './headerButtonItem';
+import * as Constants from './../core/Constants';
+import * as Messages from './../core/UIMessages';
+
 
 class Header extends Component {
     render() {
         return (
                 <div className="header-bg" >
                     <div className="flex header-position">
-                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-home" title="Home" msg="Home" />
-                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-bolt" title="Moments" msg="Moments"/>
-                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-bell" title="Notifications" msg="Notifications" />
-                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-envelope" title="Messages" msg="Messages"/>
+                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-home" title={Messages.HOME} msg={Messages.HOME} />
+                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-bolt" title={Messages.MOMENTS} msg={Messages.MOMENTS}/>
+                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-bell" title={Messages.NOTIF} msg={Messages.NOTIF} />
+                        <HeaderButtonItem profileData="dummyProfile" icon="fa fa-envelope" title={Messages.MESSAGES} msg={Messages.MESSAGES}/>
                     </div>
                     <div className="twitter-icon-style">
-                    <i className="fab fa-twitter fa-1x"/>
+                    <i className="fab fa-twitter fa-1x" alt={Messages.TITLE}/>
                 </div>
                 </div>
                 );
